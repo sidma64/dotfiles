779f00c2f8ca5f74e8e1a8f3692c86a5b0788a8e
sidma64 <toprak.code@gmail.com>
2024-09-02 15:06:49 +0300

    Install zinit

--- zsh/.config/zsh/init.zsh
+++ zsh/.config/zsh/init.zsh
@@ -1,6 +1,16 @@
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+source "${ZINIT_HOME}/zinit.zsh"
+
 autoload -U colors && colors
 export PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
 
-mkcd () {
-  mkdir $1 && cd $1
-}
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
+
+zinit load zsh-users/zsh-syntax-highlighting
+zinit load zsh-users/zsh-autosuggestions
+zinit load zsh-users/zsh-completions
+
+source functions.zsh
