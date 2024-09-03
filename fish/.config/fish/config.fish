c7ba6ebc3830acee916b65e35cc7cd666d559f1f
sidma64 <toprak.code@gmail.com>
2024-09-03 11:38:38 +0300

    Setup vi-mode in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,16 @@
+function fish_user_key_bindings
+  fish_vi_key_bindings
+end
+
+# Setup Zoxide
+zoxide init fish | source
+
+# Set up fzf key bindings
+fzf --fish | source
+
+# Go package manager installations
+set PATH $PATH:$HOME/go/bin
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
