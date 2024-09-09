415c1c65f0819d25b42f68fe792d3a82cc028593
sidma64 <toprak.code@gmail.com>
2024-09-10 02:12:11 +0300

    Add command availability checks to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,28 @@
-op completion fish | source
+# Setup completions for 1Password
+if command -q op
+  op completion fish | source
+end
+
+# Add local bin to path
 set -a PATH $HOME/.local/bin
+
 # Enable vi keybindings
 function fish_user_key_bindings
   fish_vi_key_bindings
 end
+
+# Setup cargo for rust
 set -a PATH $HOME/.cargo/bin
 
 # Setup fnm for node version manager
 fnm env --use-on-cd --shell fish | source
+
+# Setup fisher for fish plugin manager
 if command -q fisher
   echo "you need to install fisher - https://github.com/jorgebucaran/fisher#installation"
 end
 
+# Setup exa for ls
 if command -q exa
   function ls -w exa
     exa -hg --icons --group-directories-first --color=auto $argv
@@ -21,10 +32,14 @@ else
 end
 
 # Setup Zoxide
-zoxide init fish | source
+if command -q zoxide
+  zoxide init fish | source
+end
 
 # Set up fzf key bindings
-fzf --fish | source
+if command -q fzf
+  fzf --fish | source
+end
 
 # Go package manager installations
 set GOPATH $HOME/go
@@ -32,15 +47,14 @@ set GOBIN $GOPATH/bin
 set PATH $PATH $GOBIN
 
 # Set default BROWSER and EDITOR 
-
 if not set -q BROWSER
   set BROWSER firefox
 end
-
 if not set -q EDITOR
   set EDITOR nvim
 end
 
+# Set up abbreviations
 abbr --add browser $BROWSER
 abbr --add br $BROWSER
 abbr --add editor $EDITOR
---
+++ fish/.config/fish/functions/f.fish
@@ -0,0 +1,3 @@
+function f
+  cd (ls | fzf)
+end
