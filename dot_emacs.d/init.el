82c51f1091d3ae3af11060597e8e8028fa17bebe
sidma64 <toprak.code@gmail.com>
2025-02-14 14:46:50 +0300

    Add emacs config

---
+++ dot_emacs.d/init.el
@@ -0,0 +1,25 @@
+;; Initialize package system
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Ensure packages are installed
+(dolist (package '(evil vterm))
+  (unless (package-installed-p package)
+    (package-refresh-contents)
+    (package-install package)))
+
+;; Enable evil-mode
+(require 'evil)
+(evil-mode 1)
+
+;; Configure vterm
+(require 'vterm)
+(setq vterm-shell (getenv "SHELL"))
+(setq vterm-max-scrollback 10000)
+
+;; Key binding for quick terminal access
+(global-set-key (kbd "C-c t") 'vterm)
+
+;; Provide the init
+(provide 'init)
