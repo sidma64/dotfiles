82c2e87a3200f823b0e4773493ef67360cb90453
sidma64 <toprak.code@gmail.com>
2025-02-25 01:14:52 +0300

    Change emacs theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,8 +18,11 @@
 (setq vterm-shell (getenv "SHELL"))
 (setq vterm-max-scrollback 10000)
 
+(load-theme 'misterioso t)
+
 ;; Key binding for quick terminal access
 (global-set-key (kbd "C-c t") 'vterm)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Provide the init
 (provide 'init)
